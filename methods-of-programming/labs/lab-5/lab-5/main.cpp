d87db0992d11e375b717f7aefc2988ef0af5f4f6
giplash <anashkinviktar@gmail.com>
2018-12-06 23:55:55 +0300

    add lab-5

---
+++ methods-of-programming/labs/lab-5/lab-5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab-5
+//
+//  Created by Viktor Anashkin on 12/6/18.
+//  Copyright © 2018 BSU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
