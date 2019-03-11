191eb0c558ea202eea579eaa9c53ec2120e9c40a
giplash <anashkinviktar@gmail.com>
2019-03-11 23:47:57 +0300

    update lab3 java

--- methods-of-programming/Java_4sem/lab3/src/main/java/by/bsu/action/Solver.java
+++ methods-of-programming/Java_4sem/lab3/src/main/java/by/bsu/action/Solver.java
@@ -1,16 +1,14 @@
 package by.bsu.action;
 
+import java.util.*;
+
 public class Solver {
 
-    public static void sort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (Integer.toString(arr[j]).length() > Integer.toString(arr[j + 1]).length()) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+    public static int[] sort(int[] arr) {
+        return Arrays.stream(arr)
+                .boxed()
+                .sorted((a, b) -> Integer.toString(a).length() - Integer.toString(b).length())
+                .mapToInt(i -> i)
+                .toArray();
     }
 }
--- methods-of-programming/Java_4sem/lab3/src/test/java/by/bsu/action/SolverTest.java
+++ methods-of-programming/Java_4sem/lab3/src/test/java/by/bsu/action/SolverTest.java
@@ -9,33 +9,29 @@ public class SolverTest {
     @Test
     void baseTest1() {
         int[] initial = { 100, 10, 1 };
-        int[] sorted = { 1, 10, 100 };
-        Solver.sort(initial);
-        assertArrayEquals(sorted, initial);
+        int[] sorted = { 1, 10, 100 };        ;
+        assertArrayEquals(sorted, Solver.sort(initial));
     }
 
     @Test
     void baseTest2() {
         int[] initial = { 1, 102, 101, 100 };
         int[] sorted = { 1, 102, 101, 100 };
-        Solver.sort(initial);
-        assertArrayEquals(sorted, initial);
+        assertArrayEquals(sorted, Solver.sort(initial));
     }
 
     @Test
     void testWithOneValue() {
         int[] initial = { 1 };
         int[] sorted = { 1 };
-        Solver.sort(initial);
-        assertArrayEquals(sorted, initial);
+        assertArrayEquals(sorted, Solver.sort(initial));
     }
 
     @Test
     void testWithEmptyArray() {
         int[] initial = {  };
         int[] sorted = {  };
-        Solver.sort(initial);
-        assertArrayEquals(sorted, initial);
+        assertArrayEquals(sorted, Solver.sort(initial));
     }
 
 }
